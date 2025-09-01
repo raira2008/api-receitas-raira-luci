45c761ba16eff413b0dcc9500652180b6de3a1ae
lucialves1991 <lucivaneawendell@gmail.com>
2025-09-01 01:31:56 +0000

    Adição de duas receitas: Tonkatsu e pipoca doce

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+
 receitas = [
     {
         'nome' : 'Brownie',
@@ -10,6 +14,20 @@ receitas = [
         'ingredientes' : ['1 pacote de frango em tiras', '1 gema de ovo', '1 colher de sopa de mostarda Dijon', 'Suco de ½ limão', '1 pitada de pimenta', '½ xícara de óleo de girassol', '½ xícara de parmesão de boa qualidade ralado', '1 maço de alface americana ou romana lavado', '1 xícara de croutons'],
         'utensílios' : ['Bowl', 'Faca', 'Tábua de corte', 'Pegador multiuso'],
         'modo de preparo' : '1- Grelhe o frango; 2- Para fazer o molho, misture a gema de ovo, com a mostarda, o limão e a pimenta. Acrescente devagar o óleo batendo bem com um batedor de arame emulsionando essa mistura. Vai ficar um molho cremoso; 3- Acrescente metade do parmesão ralado e acerte o sal; 4- Misture a alface com esse molho, com o frango e os croutons e finalize com o restante do parmesão. Sirva.'
+
+    },
+    {
+'nome' : 'Tonkatsu',
+'ingredientes' : ['1 bisteca de porco', '2 colheres (sopa) de farinha de trigo', 'sal e pimenta-do-reino a gosto', 'molho para tonkatsu a gosto', '1 ovo levemente batido', '4 colheres (sopa) de farinha panko ou farinha de rosca', 'óleo para fritar'],
+'utensílios' : ['Frigideira', 'Tigela'],
+'modo de preparo' : '1- Tempere a bisteca de porco com sal e pimenta-do-reino; 2- Passe a bisteca no ovo levemente batido; 3- Em seguida, passe-a na farinha de trigo; 4- Passe a bisteca no ovo novamente e finalize passando na farinha de rosca ou farinha panko; 5- Aqueça uma frigideira com óleo em temperatura média; 6- Frite a bisteca até ficar dourada dos dois lados; 7- Corte-a em fatias de mais ou menos 1 dedo de largura; 8- Sirva com molho tonkatsu.'
+
+    },
+    {
+        'nome' : 'Pipoca doce',
+        'ingredientes' : ['5 colheres (sopa) de óleo', '5 colheres (sopa) de açúcar', '1 colher (sopa) de chocolate em pó', '5 colheres (sopa) de milho para pipoca', '3 colheres (sopa) de água'],
+        'utensílios' : ['Panela', 'Bowl', 'espátula de silicone', 'Pipoqueira'],
+        'modo de preparo' : '1- Em uma panela, adicione todos os ingredientes e misture delicadamente; 2- Desligue o fogo quando o intervalo de tempo entre os estouros da pipoca diminuir; 3- Está pronta para comer.'
 
     },
     {
