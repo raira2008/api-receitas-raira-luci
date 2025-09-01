fc3d8b07c8f68b2a741e81f649aca2b72c1cd7dc
Raira <raira.alves10@aluno.ifce.edu.br>
2025-09-01 01:10:40 +0000

    Criação do projeto e adição de duas receitas

---
+++ main.py
@@ -0,0 +1,18 @@
+receitas = [
+    {
+        'nome' : 'Brownie',
+        'ingredientes' : ['5 colheres de manteiga', '3 xicara de achocolatado', '12 colheres de farinha de trigo', '3 ovos', '6 colheres de açucar'],
+        'utensílios' : ['Bowl', 'Forma', 'Prato de sobremesa', 'Espatula', 'Fouet'],
+        'modo de preparo' : '1- Derreta a manteiga e reserve; 2- Enquanto derrete a manteiga, misture os 3 ovos e a açúcar e misture bem; 3- Acrescente a manteiga derretida no ovo e o açúcar; 4- Agora é so misturar o achocolatado e o trigo; 5- Unte uma forma com manteiga e achocolatado; 6- Leve ao forno a 180° C por 30 minutos'
+    },
+    {
+        'nome' : 'Salada Caesar',
+        'ingredientes' : ['1 pacote de frango em tiras', '1 gema de ovo', '1 colher de sopa de mostarda Dijon', 'Suco de ½ limão', '1 pitada de pimenta', '½ xícara de óleo de girassol', '½ xícara de parmesão de boa qualidade ralado', '1 maço de alface americana ou romana lavado', '1 xícara de croutons'],
+        'utensílios' : ['Bowl', 'Faca', 'Tábua de corte', 'Pegador multiuso'],
+        'modo de preparo' : '1- Grelhe o frango; 2- Para fazer o molho, misture a gema de ovo, com a mostarda, o limão e a pimenta. Acrescente devagar o óleo batendo bem com um batedor de arame emulsionando essa mistura. Vai ficar um molho cremoso; 3- Acrescente metade do parmesão ralado e acerte o sal; 4- Misture a alface com esse molho, com o frango e os croutons e finalize com o restante do parmesão. Sirva.'
+
+    },
+    {
+
+    }
+]
