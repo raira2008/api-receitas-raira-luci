4d6a5ffce8e6f0dfce7b369a5c1e359be133f335
Raira <raira.alves10@aluno.ifce.edu.br>
2025-10-21 12:07:18 +0000

    ajustes no código

--- main.py
+++ main.py
@@ -1,48 +1,42 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from http import HTTPStatus
+from fastapi import FastAPI, HTTPException
 from typing import List
+from .schema import ReceitaBase, Receita
 
 app = FastAPI(title= "API da Raira")
 
-class ReceitaBase(BaseModel):
-    nome: str
-    ingredientes: List[str]
-    modo_de_preparo: str 
-
-class Receita(ReceitaBase):
-    id: int
 
 receitas: List[Receita] = []
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK)
 def hello():
     return{"title" : "Livro de Receitas"}
 
-@app.get("/receitas")
+@app.get("/receitas",response_model=List[Receita], status_code=HTTPStatus.OK)
 def listar_receitas():
     return receitas
 
-@app.get("/receitas/id/{id}")
+@app.get("/receitas/id/{id}", response_model=Receita, status_code=HTTPStatus.OK)
 def get_receita_por_id(id: int):
     for receita in receitas:
         if receita.id == id:
             return receita
-    return{"receita não encontrada"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
-@app.get("/receitas/{nome_receita}")
+@app.get("/receitas/{nome_receita}", response_model=Receita, status_code=HTTPStatus.OK)
 def get_receita_por_nome(nome_receita: str):
     for receita in receitas:
         if receita.nome == nome_receita:
             return receita
     
-    return{"receita não encontrada"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
 
-@app.post("/receitas")
+@app.post("/receitas", response_model=Receita, status_code=HTTPStatus.CREATED)
 def create_receita(dados: ReceitaBase):
     for r in receitas:
         if r.nome.lower() == dados.nome.lower(): #verica se já existe uma receita com esse nome
-            return {"erro": "Já existe uma receita com esse nome"}
+            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Já existe uma receita com esse nome!")
 
     novo_id = 1 if len(receitas) == 0 else receitas[-1].id + 1
     nova_receita = Receita(
@@ -54,17 +48,16 @@ def create_receita(dados: ReceitaBase):
     receitas.append(nova_receita)
     return nova_receita
 
-@app.put("/receitas/{id}")
+@app.put("/receitas/{id}", response_model=Receita,  status_code=HTTPStatus.OK)
 def update_receita(id: int, dados: ReceitaBase):
     for r in receitas:
         if r.nome.lower() == dados.nome.lower() and r.id != id: #Verifica se existe uma receita com esse nome
-            return {"erro": "Já existe uma receita com esse nome"}
-        
+            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Já existe uma receita com esse nome!")
     if dados.nome.strip() == "" or dados.modo_de_preparo.strip() == "":
-        return {"erro": "Nome ou modo de preparo não podem ser vazios"}
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Nome ou modo de preparo não podem ser vazios!")
     for ingrediente in dados.ingredientes:
         if ingrediente.strip() == "":
-            return {"erro": "Ingredientes não podem ser vazios"}
+                raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Ingredientes não podem ser vazios!")
         
     for i in range(len(receitas)):
         if receitas[i].id == id:
@@ -76,9 +69,9 @@ def update_receita(id: int, dados: ReceitaBase):
             )
             receitas[i] = (receita_atualizada)
             return receita_atualizada
-    return {"erro": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
-@app.delete("/receitas/{id}")
+@app.delete("/receitas/{id}",response_model=Receita, status_code=HTTPStatus.OK)
 def delete_receita(id: int):
     if len(receitas) == 0:
         return {"mensagem": "Não há receitas para deletar."}
@@ -90,7 +83,7 @@ def delete_receita(id: int):
                 "receita": receita_deletada
             }
     
-    return {"mensagem": "Receita não encontrada!"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
             
 
    
---
+++ schema.py
@@ -0,0 +1,7 @@
+class ReceitaBase(BaseModel):
+    nome: str
+    ingredientes: List[str]
+    modo_de_preparo: str 
+
+class Receita(ReceitaBase):
+    id: int
